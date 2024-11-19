c866ee9b7197a3b631583ac39bb9dca01915c29c
Jerome <nietsemorej@gmail.com>
2024-11-19 09:19:25 -0800

    redirect /en-us/pages_43' to '/en-us/erc-application

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server';
 
 // Define redirect mappings
 const redirects = new Map([
-  ['/en-us/pages_43', '/en-us'],
+  ['/en-us/pages_43', '/en-us/erc-application'],
   ['/en-us/en', '/en-us'],
   ['/en-us/en/aboutus', '/en-us'],
   ['/en-us/en/Services', '/en-us/services'],
