6f0ad01203d3ed67ab6c0398d18da2100ea6773a
Jerome <nietsemorej@gmail.com>
2024-11-19 17:22:25 -0800

    fix: delete setRequestLocale at layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,31 +1,19 @@
 import Header from "@/app/components/Header"
 import Footer from "@/app/components/Footer"
 import "@/app/globals.css"
-import { type Locale, locales } from "@/i18n.config";
-import { setRequestLocale, getTranslations } from "next-intl/server";
-
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
 
 export const metadata = {
   title: 'JIAHUA US ACCOUNTING',
   description: 'Professional accounting services in the US',
 }
 
+
+
 export default function RootLayout({
   children,
-  params: {
-    locale
-  }
 }: {
   children: React.ReactNode
-  params: {
-    locale: Locale
-  }
 }) {
-  setRequestLocale(locale);
-
   return (
     <html lang="zh">
       <body className="flex flex-col min-h-screen">
