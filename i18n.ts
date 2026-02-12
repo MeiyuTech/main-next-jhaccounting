c87cc9ffd2a60ed1e37fa54345395519137d2efb
Jerome <nietsemorej@gmail.com>
2026-02-12 11:18:59 -0800

    refactor: asynchronously resolve the locale and include it in the i18n configuration.

--- i18n.ts
+++ i18n.ts
@@ -2,15 +2,16 @@ import { getRequestConfig } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { locales, type Locale } from "@/i18n.config";
 /**
- * Get the request config for the current locale    
- * @param locale - The locale to get the request config for
+ * Get the request config for the current locale
  * @returns The request config for the current locale
  */
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as Locale)) {
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+  if (!locale || !locales.includes(locale as Locale)) {
     return notFound();
   }
   return {
+    locale,
     messages: (await import(`./locales/${locale}.json`))
       .default,
   };
